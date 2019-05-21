2a30b64eef6e90413ef9ca46c51f0b7cdfb66e93
hexian <845071058@qq.com>
2019-05-21 16:37:42 +0800

    删除image

--- src/main/java/cc/shoes/controller/ImageController.java
+++ src/main/java/cc/shoes/controller/ImageController.java
@@ -1,29 +1,29 @@
-package cc.shoes.controller;
-
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import cc.shoes.service.ImageService;
-
-@Controller
-@RequestMapping("/ima")
-public class ImageController {
-
-	@Autowired
-	private ImageService imageService;
-	
-	@RequestMapping("/add")
-	public void addIma() {
-		String path = "D:/ima.jpg";
-		imageService.addImage(path);
-	}
-	
-	
-	@RequestMapping("/find")
-	public void findIma() throws IOException {
-		imageService.findIma(1);
-	}
-}
+//package cc.shoes.controller;
+//
+//import java.io.IOException;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import cc.shoes.service.ImageService;
+//
+//@Controller
+//@RequestMapping("/ima")
+//public class ImageController {
+//
+//	@Autowired
+//	private ImageService imageService;
+//	
+//	@RequestMapping("/add")
+//	public void addIma() {
+//		String path = "D:/ima.jpg";
+//		imageService.addImage(path);
+//	}
+//	
+//	
+//	@RequestMapping("/find")
+//	public void findIma() throws IOException {
+//		imageService.findIma(1);
+//	}
+//}
--- src/main/java/cc/shoes/service/ImageService.java
+++ src/main/java/cc/shoes/service/ImageService.java
@@ -1,12 +1,12 @@
-package cc.shoes.service;
-
-import java.io.IOException;
-
-import cc.shoes.entity.Image;
-
-public interface ImageService {
-
-	void addImage(String path);
-	
-	void findIma(int imageid) throws IOException;
-}
+//package cc.shoes.service;
+//
+//import java.io.IOException;
+//
+//import cc.shoes.entity.Image;
+//
+//public interface ImageService {
+//
+//	void addImage(String path);
+//	
+//	void findIma(int imageid) throws IOException;
+//}
