a80e6e42ddc91f90256bb014333820da4abbdf39
hexian <845071058@qq.com>
2019-05-22 10:56:34 +0800

    1

--- src/main/java/cc/shoes/controller/PicController.java
+++ src/main/java/cc/shoes/controller/PicController.java
@@ -3,7 +3,6 @@ package cc.shoes.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,21 +15,6 @@ import cc.shoes.service.PictureService;
 @RestController
 @RequestMapping("/pic")
 public class PicController {
-	@Value("${FTP.ADDRESS}")
-	private String host;
-	// 端口
-	@Value("${FTP.PORT}")
-	private String port;
-	@Value("${FTP.USERNAME}")
-	private String userName;
-	@Value("${FTP.PASSWORD}")
-	private String passWord;
-	// 文件在服务器端保存的主目录
-	@Value("${FTP.BASEPATH}")
-	private String basePath;
-	// 访问图片时的基础url
-	@Value("${IMAGE.BASE.URL}")
-	private String baseUrl;
 	@Autowired
 	private PictureService pictureService;
 
